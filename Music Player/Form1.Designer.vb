ab742b39ef32a2b3b56a25ddfd194557e59c38e8
sandi <sandi@LUCHSHEYE>
2023-10-01 16:37:09 -0500

    Add project files.

---
+++ Music Player/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.lstMusic = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.prbSongProgress = New System.Windows.Forms.ProgressBar()
+        Me.lblNoMusic = New System.Windows.Forms.Label()
+        Me.rtbInfo = New System.Windows.Forms.RichTextBox()
+        Me.btnSwitchSong = New System.Windows.Forms.Button()
+        Me.lblCurrentSong = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Location = New System.Drawing.Point(393, 35)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(130, 39)
+        Me.btnPlay.TabIndex = 0
+        Me.btnPlay.Text = "Play"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'lstMusic
+        '
+        Me.lstMusic.FormattingEnabled = True
+        Me.lstMusic.ItemHeight = 18
+        Me.lstMusic.Location = New System.Drawing.Point(118, 35)
+        Me.lstMusic.Name = "lstMusic"
+        Me.lstMusic.Size = New System.Drawing.Size(134, 202)
+        Me.lstMusic.TabIndex = 2
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(14, 35)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(81, 61)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "&Add Track"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'prbSongProgress
+        '
+        Me.prbSongProgress.Location = New System.Drawing.Point(278, 212)
+        Me.prbSongProgress.Name = "prbSongProgress"
+        Me.prbSongProgress.Size = New System.Drawing.Size(244, 26)
+        Me.prbSongProgress.TabIndex = 4
+        '
+        'lblNoMusic
+        '
+        Me.lblNoMusic.AutoSize = True
+        Me.lblNoMusic.BackColor = System.Drawing.SystemColors.Window
+        Me.lblNoMusic.Location = New System.Drawing.Point(147, 127)
+        Me.lblNoMusic.Name = "lblNoMusic"
+        Me.lblNoMusic.Size = New System.Drawing.Size(76, 18)
+        Me.lblNoMusic.TabIndex = 5
+        Me.lblNoMusic.Text = "No music."
+        '
+        'rtbInfo
+        '
+        Me.rtbInfo.BackColor = System.Drawing.SystemColors.Control
+        Me.rtbInfo.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.rtbInfo.Location = New System.Drawing.Point(267, 35)
+        Me.rtbInfo.Name = "rtbInfo"
+        Me.rtbInfo.Size = New System.Drawing.Size(100, 150)
+        Me.rtbInfo.TabIndex = 6
+        Me.rtbInfo.Text = ""
+        '
+        'btnSwitchSong
+        '
+        Me.btnSwitchSong.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSwitchSong.Location = New System.Drawing.Point(393, 81)
+        Me.btnSwitchSong.Name = "btnSwitchSong"
+        Me.btnSwitchSong.Size = New System.Drawing.Size(132, 26)
+        Me.btnSwitchSong.TabIndex = 7
+        Me.btnSwitchSong.Text = "Switch Song"
+        Me.btnSwitchSong.UseVisualStyleBackColor = True
+        '
+        'lblCurrentSong
+        '
+        Me.lblCurrentSong.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCurrentSong.Location = New System.Drawing.Point(278, 255)
+        Me.lblCurrentSong.Name = "lblCurrentSong"
+        Me.lblCurrentSong.Size = New System.Drawing.Size(244, 18)
+        Me.lblCurrentSong.TabIndex = 8
+        Me.lblCurrentSong.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(537, 282)
+        Me.Controls.Add(Me.lblCurrentSong)
+        Me.Controls.Add(Me.btnSwitchSong)
+        Me.Controls.Add(Me.rtbInfo)
+        Me.Controls.Add(Me.lblNoMusic)
+        Me.Controls.Add(Me.prbSongProgress)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstMusic)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Custom Media Player"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPlay As Button
+    Friend WithEvents lstMusic As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents prbSongProgress As ProgressBar
+    Friend WithEvents lblNoMusic As Label
+    Friend WithEvents rtbInfo As RichTextBox
+    Friend WithEvents btnSwitchSong As Button
+    Friend WithEvents lblCurrentSong As Label
+End Class
---
+++ Music Player/Form1.vb
@@ -0,0 +1,258 @@
+﻿
+Imports System.IO
+Imports System.Reflection.Emit
+Imports System.Runtime.Serialization.Formatters
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Header
+Imports TagLib.Asf
+Imports WMPLib, TagLib, System.Reflection
+
+Public Class Form1
+
+    Public Class Song
+        Public Property FilePath As String
+        Public Property Name As String
+        Public Property Artist As String
+        Public Property Album As String
+        Public Property Length As TimeSpan
+        Public Property dblLength As Double
+        Public Property Index As Integer
+
+        Public Sub New(filePath As String, name As String, artist As String, album As String, length As TimeSpan, dblLength As Double, myIndex As Integer)
+            Me.FilePath = filePath
+            Me.Name = name
+            Me.Artist = artist
+            Me.Album = album
+            Me.Length = length
+            Me.dblLength = dblLength
+            Me.Index = myIndex
+        End Sub
+
+    End Class
+
+    Public Property SongPlayedPaused As Boolean
+        Get
+            Return songPlaying
+        End Get
+
+        Set(value As Boolean)
+            If value <> songPlaying Then
+                songPlaying = value
+                If songPlaying = False Then
+                    btnPlay.Text = "Play"
+                Else
+                    btnPlay.Text = "Pause"
+                End If
+
+
+            End If
+        End Set
+    End Property
+
+
+
+    ' -------------------------- Globals ------------------------------------------
+
+    Public Event ItemAddedToListBox(theListBox As ListBox)
+
+    Dim milliseconds As Integer
+    Dim WithEvents timer As New Timer
+    Private firstSongPlayed As Boolean
+    Private currentSong As Song
+    Private songPosition As Double
+    Private songPlaying As Boolean
+    Private songList As New List(Of Song)
+    Private mp3Play As WindowsMediaPlayer = New WindowsMediaPlayer
+
+    ' -------------------------- End Globals ------------------------------------------
+
+    ' ------------------------------------ Handlers -----------------------------------------------------
+
+    Private Sub ListBoxItemAddedHandler(theListBox As ListBox) Handles Me.ItemAddedToListBox
+        If (theListBox.Items).Count <> 0 Then
+            lblNoMusic.Visible = False
+        Else
+            lblNoMusic.Visible = True
+        End If
+    End Sub
+
+    Private Sub lstMusic_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstMusic.SelectedIndexChanged
+        If lstMusic.SelectedIndex > -1 Then
+            Dim currentItem As Song
+
+            currentItem = songList(lstMusic.SelectedIndex)
+            rtbInfo.Visible = True
+
+            Dim rtfText As String
+
+            rtfText += ($" \b Song\b0: {currentItem.Name}\par")
+            rtfText += ($" \b Artist\b0: {currentItem.Artist}\par")
+            rtfText += ($" \b Album\b0: {currentItem.Name}\par")
+            rtfText += ($" \b Length\b0: {currentItem.Length.ToString}\par")
+
+            rtbInfo.Rtf = ConvertToRTF(rtfText)
+
+            If firstSongPlayed = True Then
+                If lstMusic.SelectedIndex <> currentSong.Index Then
+                    btnSwitchSong.Visible = True
+                Else
+                    btnSwitchSong.Visible = False
+                End If
+            End If
+
+
+
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rtbInfo.Visible = False
+        btnSwitchSong.Visible = False
+
+    End Sub
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+
+
+        If lstMusic.SelectedIndex > -1 Then
+
+            If lstMusic.Items.Count = 1 And lstMusic.SelectedIndex = 0 And firstSongPlayed = False Then
+                firstSongPlayed = True
+                mp3Play.URL = songList(lstMusic.SelectedIndex).FilePath
+                mp3Play.controls.stop()
+                initializeProgressBar(songList(lstMusic.SelectedIndex))
+            End If
+
+            If btnPlay.Text = "Pause" Then
+                mp3Play.controls.pause()
+                Me.SongPlayedPaused = False
+                songPosition = mp3Play.controls.currentPosition
+            Else
+                If mp3Play.URL = songList(lstMusic.SelectedIndex).FilePath Then
+                    currentSong = songList(lstMusic.SelectedIndex)
+                    lblCurrentSong.Text = currentSong.Name
+
+                End If
+                mp3Play.controls.play()
+                Me.SongPlayedPaused = True
+                mp3Play.controls.currentPosition = songPosition
+            End If
+        Else
+            MsgBox("No song is selected!")
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim openFileDialog As New OpenFileDialog()
+
+        openFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3|All Files (*.*)|*.*"
+        openFileDialog.Title = "Select an MP3 File"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            updateMusic(openFileDialog)
+        End If
+    End Sub
+
+    Private Sub btnSwitchSong_Click(sender As Object, e As EventArgs) Handles btnSwitchSong.Click
+        mp3Play.URL = songList(lstMusic.SelectedIndex).FilePath
+        mp3Play.controls.stop()
+        Me.SongPlayedPaused = False
+
+        initializeProgressBar(songList(lstMusic.SelectedIndex))
+
+        btnSwitchSong.Visible = False
+    End Sub
+
+
+    ' ------------------------------------ End Handlers -----------------------------------------------------
+
+    ' ----------------------------------------- Functions/Subs ----------------------------------------------------- 
+
+    Private Function ConvertToRTF(ByVal message As String) As String
+        Dim rtfText As String = "{\rtf1\ansi\deff0{\fonttbl{\f0 Arial;}}"
+        rtfText &= "\viewkind4\uc1\pard\lang1033\f0\fs20" & message & "\par}"
+        Return rtfText
+    End Function
+
+    Private Sub updateMusic(openFileDialog As OpenFileDialog)
+        Dim filePath As String = openFileDialog.FileName
+
+        songList.Add(makeSong(openFileDialog))
+
+        lstMusic.Items.Clear()
+        For Each song As Song In songList
+            If Not String.IsNullOrEmpty(song.Artist) Then
+                lstMusic.Items.Add($"{song.Name} - {song.Artist}")
+            Else
+                lstMusic.Items.Add($"{song.Name}")
+            End If
+        Next
+
+        RaiseEvent ItemAddedToListBox(lstMusic)
+    End Sub
+
+    Private Function makeSong(openFileDialog As OpenFileDialog)
+        Static songsAdded As Integer = -1
+        songsAdded += 1
+        Dim filePath As String = openFileDialog.FileName
+        Dim currentSong As TagLib.File = TagLib.File.Create(filePath)
+
+        Dim title As String
+
+        If Not (String.IsNullOrEmpty(currentSong.Tag.Title)) Then
+            title = currentSong.Tag.Title
+        Else
+            title = filePath.Substring(filePath.LastIndexOf("\") + 1)
+            title = title.Substring(0, title.Length - 4)
+        End If
+
+        Dim artist As String = currentSong.Tag.FirstPerformer
+        Dim album As String = currentSong.Tag.Album
+        Dim length As TimeSpan = currentSong.Properties.Duration
+
+        Debug.Print($"Title: {title}, Artist: {artist}, Album: {album}, Length: {length}")
+
+
+        Dim newSong As New Song(filePath, title, artist, album, length, length.TotalSeconds, songsAdded)
+        currentSong.Dispose()
+
+        Return newSong
+    End Function
+
+    Private Sub initializeProgressBar(currentSong As Song)
+
+        prbSongProgress.Maximum = CInt(currentSong.dblLength)
+        prbSongProgress.Step = 1
+
+        Debug.Print($"Song Length = {CInt(currentSong.dblLength)}, Maximum = {prbSongProgress.Maximum}, Step = {prbSongProgress.Step}, Width = {prbSongProgress.Width}")
+
+        timer.Stop()
+        milliseconds = 0
+        timer.Start()
+
+    End Sub
+
+    Private Sub Timer_Tick(sender As System.Object, e As System.EventArgs) Handles timer.Tick
+        prbSongProgress.Value = mp3Play.controls.currentPosition
+
+        If prbSongProgress.Value >= prbSongProgress.Maximum Then
+            timer.Stop()
+        End If
+
+    End Sub
+
+    Private Sub prbSongProgress_Click(sender As Object, e As EventArgs) Handles prbSongProgress.Click
+        Dim clickPosition As Integer = prbSongProgress.PointToClient(MousePosition).X
+        Dim value As Integer = CInt((clickPosition / prbSongProgress.Width) * prbSongProgress.Maximum)
+
+        prbSongProgress.Value = value
+        mp3Play.controls.currentPosition = value
+    End Sub
+
+
+
+
+    ' ----------------------------------------- End Functions/Subs ----------------------------------------------------- 
+
+End Class
